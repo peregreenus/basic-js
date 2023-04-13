95a852ef4cf5c129d2ea40c3a8d0ed0486df3824
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2023-04-13 12:25:39 +0300

    refactor: task count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,17 +14,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
+// function countCats(matrix) {
+//   const cat = '^^';
+//   let counter = 0;
+
+//   for (let i = 0; i < matrix.length; i++) {
+//     for (let j = 0; j < matrix[i].length; j++) {
+//       if (matrix[i][j] === cat) counter++;
+//     }
+//   }
+
+//   return counter;
+// }
+
 function countCats(matrix) {
-  const cat = '^^';
-  let counter = 0;
-
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] === cat) counter++;
-    }
-  }
-
-  return counter;
+  return matrix.flat().reduce((acc, curr) => acc + (curr === '^^' ? 1 : 0), 0);
 }
 
 module.exports = {
