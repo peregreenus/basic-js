dc9c1bb91fda326fb434a8ec337cf3b5e8174755
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2023-04-13 15:19:39 +0300

    refactor: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -19,18 +19,6 @@ function createDreamTeam(members) {
     .map(i => i.trim().toUpperCase())
     .sort()
     .reduce((acc, curr) => acc + curr[0], '') : false;
-  // let result = '';
-  // members.sort();
-
-  // for (let i = 0; i < members.length; i++) {
-  //   if (typeof members[i] === 'string' && isNaN(+members[i])) {
-  //     result += members[i].trim()[0].toUpperCase();
-  //   } else {
-  //     continue;
-  //   }
-  // }
-
-  // return result;
 }
 
 module.exports = {
