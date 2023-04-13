ea371fd7085c0fb103e639e4b727ef21e27a1989
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2023-04-13 23:12:12 +0300

    refactor: task Hanoi tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -16,7 +16,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function calculateHanoi(disksNumber, turnsSpeed) {
   const turns = Math.pow(2, disksNumber) - 1;
-  const seconds = Math.floor(turns * 3600 / turnsSpeed);
+  const seconds = Math.floor((turns / turnsSpeed) * 3600);
   return {
     turns,
     seconds
